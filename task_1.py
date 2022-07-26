eb8f00ddc42e5949e4bf1b3f8e7ea54365df31ab
Иван Сапронов <ivnnis@gmail.com>
2022-07-26 13:00:10 +0300

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,6 +1,13 @@
-def task(numbers: str) -> int:
+def find_first_zero(numbers: str) -> int:
     return numbers.find('0')
 
 
-print(task('111111111110000000000000000')) # 11
-print(task('111100000000')) # 4
+
+tests = [
+    {'data': '111111111110000000000000000', 'answer': 11},
+    {'data': '111100000000', 'answer': 5}
+]
+if __name__ == '__main__':
+   for i, test in enumerate(tests):
+       test_answer = find_first_zero(test['data'])
+       assert test_answer == test['answer'], f'Error on test case {i}, got {test_answer}, expected {test["answer"]}'
